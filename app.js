67866bf1559cb7e5cfcb647b9818639345823558
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2020-11-14 00:18:02 +0530

    set for production

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 //const MONGO_URI = config.get("mongoURI");
 require("dotenv").config();
 const cors = require("cors");
@@ -61,6 +62,16 @@ app.get("/api/", (req, res) => {
 app.use("/api/projects/", projectRoutes);
 app.use("/api/auth/", authRoutes);
 app.use("/api/user/", userRoutes);
+
+//Serve static assets if in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
